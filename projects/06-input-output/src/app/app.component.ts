ad897839c7c2246adb5e778b0b73512747559c3e
Marios Stefanidis <mstefanidis@knowledge.gr>
2026-05-28 14:22:42 +0300

    Enhance car listing functionality with dynamic components
    - Introduced ListingComponent to encapsulate car details and handle car saving.
    - Updated app.component.ts to utilize ListingComponent for rendering car listings dynamically.
    - Implemented event emission for saving cars to the savedCarList.

--- projects/06-input-output/src/app/app.component.ts
+++ projects/06-input-output/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Car } from './car';
+import { ListingComponent } from './listing/listing.component';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +9,19 @@ import { Car } from './car';
     <h1>Saved Cars {{ savedCarList.length }}</h1>
     <section class="container">
       <!-- This article element represents and entire listing -->
-      <article class="listing">
-        <div class="image-parent">
-          <img class="product-image" src="https://placehold.co/100x100" />
-        </div>
-        <section class="details">
-          <p class="title"><!-- car make and model--></p>
-          <hr />
-          <p class="detail">
-            <span>Year</span>
-            <span><!-- year --></span>
-          </p>
-          <div class="detail">
-            <span>Transmission</span>
-            <span><!-- transmission --></span>
-          </div>
-          <p class="detail">
-            <span>Mileage</span>
-            <span><!-- miles --></span>
-          </p>
-          <p class="detail">
-            <span>Price</span>
-            <span><!-- price --></span>
-          </p>
-        </section>
-      </article>
+      @for (carEntry of carList; track carEntry) {
+        <app-listing [car]="carEntry" (carSaved)="addCarToSaved($event)" />
+      }
       <!-- end car listing markup -->
     </section>
+    <article>
+      @for (savedCarEntry of savedCarList; track savedCarEntry) {
+        <p>{{ savedCarEntry.make }} {{ savedCarEntry.model }}</p>
+      }
+    </article>
   `,
   styles: [],
+  imports: [ListingComponent],
 })
 export class AppComponent {
   savedCarList: Car[] = [];
@@ -74,4 +59,8 @@ export class AppComponent {
       transmission: 'Automatic',
     },
   ];
+
+  addCarToSaved(car: Car) {
+    this.savedCarList.push(car);
+  }
 }
