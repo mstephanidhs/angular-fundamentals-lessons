2e978dc5c41fd66efecbe2f71a6bb8c36ee20a33
Marios Stefanidis <mstefanidis@knowledge.gr>
2026-06-02 12:01:32 +0300

    Refactor app.component.ts and user service for user data handling
    - Updated app.component.ts to implement OnInit lifecycle hook for fetching user data asynchronously.
    - Integrated UserService to retrieve user data and display it using UserInfoComponent.
    - Enhanced user-info.component.ts to accept user data via @Input decorator.
    - Added Injectable decorator to UserService for proper dependency injection.

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: `
+    <h1>User Listing</h1>
+    @for (user of userData; track user.id) {
+      <app-user-info [user]="user" />
+    }
+  `,
 })
-export class AppComponent {
-  constructor() {}
+// export class AppComponent {
+//   userService = inject(UserService);
+//   userData: User[] = [];
+
+//   constructor() {
+//     this.userService.getUserData().then((data) => (this.userData = data));
+//   }
+// }
+export class AppComponent implements OnInit {
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  // ngOnInit is a lifecycle hook that is called after the constructor and the OnChanges hook
+  async ngOnInit(): Promise<void> {
+    const data = await this.userService.getUserData();
+    this.userData = data;
+  }
 }
--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
+// Before signals, Angualar used zones for change detection
+
+// 3 reactive primities: signal, computed, effect
+// Based on memoization and caching strategies
+
+// signal: a value that can tell Angular when it changes - capable of notifying 
+// its context of future changes in its value
+
+// computed: derive new value when one of the dependent signals change
+
+// effect: is a side-effectful operation which reads 
+// the value of zero or more signals
+// make something happen when one of the signals changes
+
 @Component({
   selector: 'app-root',
   standalone: true,
