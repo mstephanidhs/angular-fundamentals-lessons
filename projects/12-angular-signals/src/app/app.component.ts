79e8f4696366fc2ae25952e66565534db08f93bf
Marios Stefanidis <mstefanidis@knowledge.gr>
2026-06-02 13:20:31 +0300

    Implement reactive state management in app.component.ts
    - Introduced signals and computed properties for managing todo list state.
    - Updated the template to display completed todos and bind checkbox inputs for updating todo completion status.
    - Enhanced updateTodo method to toggle completion state of todos.

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -7,12 +7,12 @@ import { Todo } from './todo';
 // 3 reactive primities: signal, computed, effect
 // Based on memoization and caching strategies
 
-// signal: a value that can tell Angular when it changes - capable of notifying 
+// signal: a value that can tell Angular when it changes - capable of notifying
 // its context of future changes in its value
 
 // computed: derive new value when one of the dependent signals change
 
-// effect: is a side-effectful operation which reads 
+// effect: is a side-effectful operation which reads
 // the value of zero or more signals
 // make something happen when one of the signals changes
 
@@ -22,9 +22,62 @@ import { Todo } from './todo';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1>Building a TODO List</h1> `,
-  styles: `label { display: block }`,
+  template: `
+    <h1>Building a TODO List</h1>
+    <p>
+      Completed todos: {{ completed() }}
+      @for (todo of todos(); track todo.id) {
+        <label
+          [ngStyle]="{
+            'text-decoration': todo.completed ? 'line-through' : 'none',
+          }"
+        >
+          <input
+            type="checkbox"
+            [checked]="todo.completed"
+            (change)="updateTodo(todo)"
+          />
+          {{ todo.title }}
+        </label>
+      }
+    </p>
+  `,
+  styles: `
+    label {
+      display: block;
+    }
+  `,
 })
 export class AppComponent {
-  updateTodo(todo: Todo) {}
+  completed = computed(
+    () => this.todos().filter((todo) => todo.completed).length,
+  );
+
+  todos = signal<Todo[]>([
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn TypeScript',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn RxJS',
+      completed: false,
+    },
+  ]);
+
+  updateTodo(todo: Todo) {
+    this.todos.update((oldTodos) =>
+      oldTodos.map((todoEntry) =>
+        todoEntry.id === todo.id
+          ? { ...todoEntry, completed: !todoEntry.completed }
+          : todoEntry,
+      ),
+    );
+  }
 }
